c4c47087352d815e031f69450b9f1bb0edcaa877
Fabiano Dias <fabianofda@gmail.comm>
2023-10-11 17:45:09 -0300

    organizando os import ChallengingPage

--- src/test/java/projetoHibridoDeAutonacaoPB/web/pages/ChallengingPage.java
+++ src/test/java/projetoHibridoDeAutonacaoPB/web/pages/ChallengingPage.java
@@ -3,7 +3,7 @@ package projetoHibridoDeAutonacaoPB.web.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.List;
 

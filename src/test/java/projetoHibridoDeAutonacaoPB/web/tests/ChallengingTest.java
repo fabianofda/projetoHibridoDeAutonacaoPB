05b56795e8e16c7a6c0f50ed93c739d33826fd6a
Fabiano Dias <fabianofda@gmail.comm>
2023-10-13 15:17:37 -0300

    organizando os import

--- src/test/java/projetoHibridoDeAutonacaoPB/web/tests/ChallengingTest.java
+++ src/test/java/projetoHibridoDeAutonacaoPB/web/tests/ChallengingTest.java
@@ -2,16 +2,9 @@ package projetoHibridoDeAutonacaoPB.web.tests;
 
 
 import org.junit.Test;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import projetoHibridoDeAutonacaoPB.web.pages.ChallengingPage;
-
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.junit.Assert.*;
 
 public class ChallengingTest extends BaseTest{
